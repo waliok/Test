d2494b0f335cb93323a132490c76fd7b8ba1f981
waliok <105702456+waliok@users.noreply.github.com>
2025-10-20 23:37:11 +0200

    Initial commit

---
+++ TestForInterview/Helper.swift
@@ -0,0 +1,14 @@
+//
+//  Helper.swift
+//  TestForInterview
+//
+//  Created by Sam Titovskyi on 18.08.2025.
+//
+
+import Foundation
+
+class Helper {
+    
+    static let apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDUzYjRiNWExNTE4Y2JmOTEzMmU2MTU2Mzk1YmUzYiIsIm5iZiI6MTc0NDExMTc2NS4xOTkwMDAxLCJzdWIiOiI2N2Y1MDg5NWVkZGVjMjhiMDNhZGQ1ZjUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.xhPFttHQXEVDOAv7kOWJddyAJdJjBJV2UP-2xMEwLcs"
+    
+}
